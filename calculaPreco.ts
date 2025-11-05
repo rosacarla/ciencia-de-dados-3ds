4c81014210b4c1673f17bcdddea05e1d7200674f
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2025-11-05 00:26:19 -0300

    Update calculaPreco.ts

--- calculaPreco.ts
+++ calculaPreco.ts
@@ -17,4 +17,5 @@ const produto1: Produto = {
   preco: 200,
   desconto: 15
 };
+console.log(`Preço inicial de ${produto1.nome}: R$ ${produto1.preco.toFixed(2)}`);
 console.log(`Preço final de ${produto1.nome}: R$ ${calcularPrecoFinal(produto1).toFixed(2)}`);
