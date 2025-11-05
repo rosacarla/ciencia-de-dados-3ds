845b9d2251602535a19113e481c1075a7e76b42f
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2025-11-05 00:43:54 -0300

    Create calculaPreco.py

---
+++ calculaPreco.py
@@ -0,0 +1,7 @@
+# Define função para calcuclar preço final
+def calcular_preco_final(nome, preco, desconto=0):
+    preco_final = preco - (preco * desconto / 100)
+    print(f"Preço inicial de {nome} R$ {preco:.2f}")
+
+# Exemplo de uso
+calcular_preco_final("Fone de ouvido", 200, 15)
