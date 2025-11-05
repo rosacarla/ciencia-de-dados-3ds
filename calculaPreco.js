167a07e89f129752194fe4abd13142c2e65a1f64
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2025-11-05 00:51:14 -0300

    Update calculaPreco.js

--- calculaPreco.js
+++ calculaPreco.js
@@ -1,6 +1,7 @@
 function calcularPrecoFinal(produto) {
   const desconto = produto.desconto ?? 0;
   const precoFinal = produto.preco - (produto.preco * desconto / 100);
+  console.log(`Preço incial de ${produto.nome}: R$ ${produto.preco.toFixed(2)}`);
   console.log(`Preço final de ${produto.nome}: R$ ${precoFinal.toFixed(2)}`);
 }
 
@@ -8,7 +9,7 @@ function calcularPrecoFinal(produto) {
 const produto1 = {
   nome: "Fone de ouvido",
   preco: 200,
-  desconto: 15
+  desconto: 25
 };
 
 calcularPrecoFinal(produto1);
