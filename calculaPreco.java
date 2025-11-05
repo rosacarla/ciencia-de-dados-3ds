6a9bbb5e05f7449c9718b52664708f1a05bd44a6
Carla Edila Silveira <rosa.carla@pucpr.edu.br>
2025-11-05 00:57:01 -0300

    Create calculaPreco.java

---
+++ calculaPreco.java
@@ -0,0 +1,33 @@
+public class Produto {
+
+    String nome;
+
+    double preco;
+
+    double desconto;
+
+    public Produto(String nome, double preco, double desconto) {
+
+        this.nome = nome;
+
+        this.preco = preco;
+
+        this.desconto = desconto;
+
+    }
+
+    public double calcularPrecoFinal() {
+
+        return preco - (preco * desconto / 100);
+
+    }
+
+    public static void main(String[] args) {
+
+        Produto produto = new Produto("Fone de ouvido", 200, 15);
+
+        System.out.printf("Preço final de %s: R$ %.2f%n", produto.nome, produto.calcularPrecoFinal());
+
+    }
+
+}
